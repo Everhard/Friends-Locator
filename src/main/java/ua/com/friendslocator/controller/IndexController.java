b36c4e8d7042ff69b025caafeda4504a3700f2b6
Cherepanov Roman <romach007@gmail.com>
2015-12-14 22:11:56 +0200

    Add Spring MVC skeleton with Travis integration

---
+++ src/main/java/ua/com/friendslocator/controller/IndexController.java
@@ -0,0 +1,13 @@
+package ua.com.friendslocator.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping("/")
+    public String showIndex(Model model){
+        return "index";
+    }
+}
